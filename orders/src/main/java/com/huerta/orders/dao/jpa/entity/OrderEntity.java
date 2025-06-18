cd5ee772c3c5535036504cfc3ae2604ff2fa742b
Israel Huerta <israelhf24@gmail.com>
2025-06-18 16:03:34 -0600

    feat: Implement OrderService and OrderServiceImpl for order placement functionality

--- orders/src/main/java/com/huerta/orders/dao/jpa/entity/OrderEntity.java
+++ orders/src/main/java/com/huerta/orders/dao/jpa/entity/OrderEntity.java
@@ -1,30 +1,36 @@
 package com.huerta.orders.dao.jpa.entity;
 
+import java.util.UUID;
+
 import com.huerta.core.types.OrderStatus;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import java.util.UUID;
+import lombok.Getter;
+import lombok.Setter;
 
 @Table(name = "orders")
 @Entity
+@Getter
+@Setter
 public class OrderEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
 
-    @Column(name = "status")
-    private OrderStatus status;
+  @Column(name = "status")
+  private OrderStatus status;
 
-    @Column(name = "customer_id")
-    private UUID customerId;
+  @Column(name = "customer_id")
+  private UUID customerId;
 
-    @Column(name = "product_id")
-    private UUID productId;
+  @Column(name = "product_id")
+  private UUID productId;
 
-    @Column(name = "product_quantity")
-    private Integer productQuantity;
+  @Column(name = "product_quantity")
+  private Integer productQuantity;
 }
