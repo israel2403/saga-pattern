7135f062f25af5d029efc23ff52987bb392edeeb
Israel Huerta <israelhf24@gmail.com>
2025-06-19 12:26:49 -0600

    feat: Add products module with initial configuration and application setup

---
+++ products/src/test/java/com/huerta/products/ProductsApplicationTests.java
@@ -0,0 +1,11 @@
+package com.huerta.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
