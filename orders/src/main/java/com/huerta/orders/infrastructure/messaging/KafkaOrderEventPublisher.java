ffae7c3a688fee6724a34c39e1200daddecab2d6
Israel Huerta <israelhf24@gmail.com>
2025-06-25 22:30:28 -0600

    feat: Enhance order event publishing with error handling and validation

--- orders/src/main/java/com/huerta/orders/infrastructure/messaging/KafkaOrderEventPublisher.java
+++ orders/src/main/java/com/huerta/orders/infrastructure/messaging/KafkaOrderEventPublisher.java
@@ -1,6 +1,7 @@
 package com.huerta.orders.infrastructure.messaging;
 
 import com.huerta.core.dto.events.OrderCreatedEvent;
+import com.huerta.orders.infrastructure.exception.EventPublishingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -16,10 +17,15 @@ public class KafkaOrderEventPublisher {
     private String ordersEventsTopicName;
 
     public void publishOrderCreated(final OrderCreatedEvent orderCreatedEvent) {
-        if (orderCreatedEvent != null) {
-            kafkaTemplate.send(ordersEventsTopicName, orderCreatedEvent);
-        } else {
+        if (orderCreatedEvent == null) {
             throw new IllegalArgumentException("OrderCreatedEvent cannot be null");
+        }
+
+        try {
+            kafkaTemplate.send(ordersEventsTopicName, orderCreatedEvent);
+        } catch (Exception e) {
+            throw new EventPublishingException(
+                    "Failed to publish OrderCreatedEvent to topic:" + ordersEventsTopicName, e);
         }
     }
 }
